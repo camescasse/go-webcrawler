b1ce1fb2bcb473b9fa1b2068dadd914ff78154d9
camescasse <e.camescasse@gmail.com>
2024-11-11 16:18:22 -0400

    refactor getURLsFromHTML function

--- get_urls_from_html.go
+++ get_urls_from_html.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/url"
 	"strings"
 
@@ -8,41 +9,38 @@ import (
 )
 
 func getURLsFromHTML(htmlBody, rawBaseURL string) ([]string, error) {
-	doc, err := html.Parse(strings.NewReader(htmlBody))
+	baseURL, err := url.Parse(rawBaseURL)
 	if err != nil {
-		return []string{}, err
+		return nil, fmt.Errorf("couldn't parse url: %w", err)
 	}
 
-	hrefs := []string{}
-	var traverse func(*html.Node)
-	traverse = func(n *html.Node) {
-		if n.Type == html.ElementNode && n.Data == "a" {
-			for _, attribute := range n.Attr {
-				if attribute.Key == "href" {
-					hrefs = append(hrefs, attribute.Val)
-					break
+	doc, err := html.Parse(strings.NewReader(htmlBody))
+	if err != nil {
+		return nil, fmt.Errorf("couldn't parse html: %w", err)
+	}
+
+	urls := []string{}
+	var traverseNode func(*html.Node)
+	traverseNode = func(node *html.Node) {
+		if node.Type == html.ElementNode && node.Data == "a" {
+			for _, anchor := range node.Attr {
+				if anchor.Key == "href" {
+					href, err := url.Parse(anchor.Val)
+					if err != nil {
+						fmt.Printf("couldn't parse href '%v': %v", anchor.Val, err)
+						continue
+					}
+
+					resolvedURL := baseURL.ResolveReference(href)
+					urls = append(urls, resolvedURL.String())
 				}
 			}
 		}
-		for child := n.FirstChild; child != nil; child = child.NextSibling {
-			traverse(child)
+		for child := node.FirstChild; child != nil; child = child.NextSibling {
+			traverseNode(child)
 		}
 	}
-	traverse(doc)
-
-	urls := []string{}
-	for _, nl := range urls {
-		parsed, err := url.Parse(nl)
-		if err != nil {
-			return []string{}, err
-		}
-
-		if parsed.Host == "" {
-			urls = append(urls, rawBaseURL+parsed.Path)
-		} else {
-			urls = append(urls, parsed.Scheme+parsed.Host+parsed.Path)
-		}
-	}
+	traverseNode(doc)
 
 	return urls, nil
 }
